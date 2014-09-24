9a7d3dd675b24eb5c812f81e659cdea8d8b0d4dd
Melissa <simmsmj@miamioh.edu>
2014-09-24 18:05:40 -0400

    Program is now in complete working order. Use test_input.txt and compare expected_output.txt and actual_output.txt to verify. Modifications may be made to files used for testing. NOTE: spaces exist at end of row for each matrix.

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+  // Setup and initialization
   vector< vector<int> > matrixA;
   vector< vector<int> > matrixB;
   vector< vector<int> > matrixC;
@@ -15,25 +16,37 @@ int main()
   int Crow = 0;
   int Ccol = 0;
 
-  cout << "Rows for matrix A: ";
+  // For purposes of manual testing, prompts were used for cleaner input
+  // For automatic testing (and for final turn in) prompts will be commented out.
+  // cout << "Rows for matrix A: ";
   cin >> Arow;
-  cout << "Columns for matrix A: ";
+  // cout << "Columns for matrix A: ";
   cin >> Acol;
-  cout << "Rows for matrix B: ";
+  // cout << "Rows for matrix B: ";
   cin >> Brow;
-  cout << "Columns for matrix B: ";
+  // cout << "Columns for matrix B: ";
   cin >> Bcol;
 
+  // Test for bad input; If rows for matrix A and rows for matrix B do not match, stop and return 1
   if (Acol != Brow)
     {
       cout << "Invalid input\n";
       return 1;
     }
 
+  // Assign rows and columns for new matrix (C)
   Crow = Arow;
   Ccol = Bcol;
 
-  cout << "Enter values for matrix A: " << endl;
+  /*
+    Input algorithm:
+    2D vectors were declared at start of program. Since a 2D vector is a vector of vectors, 
+    each row vector is populated using STDIN and push_back()
+    The row vectors are then added to the matrix with push_back()
+   */
+
+  // Input for matrix A. Prompts commented out for final turn in.
+  // cout << "Enter values for matrix A: " << endl;
   for(int r = 0; r < Arow; r++)
     {
       vector<int> row;
@@ -46,7 +59,8 @@ int main()
       matrixA.push_back(row);
     }
   
-  cout << "Enter values for matrix B: " << endl;
+  // Input for matrix B. Prompts commented out for final turn in.
+  // cout << "Enter values for matrix B: " << endl;
   for(int r = 0; r < Brow; r++)
     {
       vector<int> row;
@@ -59,6 +73,15 @@ int main()
       matrixB.push_back(row);
     }
 
+  /*
+    Multiplication algorithm:
+    New matrix has rows and columns Crow and Ccol (init above)
+    As before, row vectors are declared and populated with column values
+    Column values are computed by finding the sum of products of A row and B col
+    sum is pushed back onto the row vector
+    row is added to resulting matrix with push_back()
+   */
+ 
   // Create product matrix
   for(int r = 0; r < Crow; r++)
     {
@@ -74,8 +97,9 @@ int main()
 	}
       matrixC.push_back(row);
     }
-
-  cout << "Printing matrix A: " << endl;
+  
+  // Printing. Prompts commented out for final turn in
+  // cout << "Printing matrix A: " << endl;
   for(int r = 0; r < Arow; r++)
     {
       for(int c = 0; c < Acol; c++)
@@ -84,9 +108,10 @@ int main()
 	}
       cout << "\n";
     }
-  cout << "\n";
+  // Newline added for clarity
+  // cout << "\n";
 
-  cout << "Printing matrix B: " << endl;
+  // cout << "Printing matrix B: " << endl;
   for(int r = 0; r < Brow; r++)
     {
       for(int c = 0; c < Bcol; c++)
@@ -95,9 +120,10 @@ int main()
 	}
       cout << "\n";
     }
-  cout << "\n";
+  // Newline added for clarity
+  // cout << "\n";
 
-  cout << "Printing matrix C: " << endl;
+  // cout << "Printing matrix C: " << endl;
   for(int r = 0; r < Crow; r++)
     {
       for(int c = 0; c < Ccol; c++)
