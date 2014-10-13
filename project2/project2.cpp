511638ed0bacc5bb8c388dbc21cf162bcb2940c1
Melissa <simmsmj@miamioh.edu>
2014-10-13 09:22:13 -0400

    Now takes scores and stores them properly; exception handling not fully funtional

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <iomanip>
 #include <stdexcept>
 #include <vector>
@@ -30,22 +31,51 @@ int main()
   string name = "";
   float score = 0.0;
   char grade = 'A';
- do
+  while(true)
     {
       cout << "Enter student name (enter none to quit): ";
-      getline(cin,name);
+      getline(cin, name);
+      if(name.compare("none") == 0)
+	{
+	  break;
+	}
+      cin.clear();
+      cin.sync();
+      score = getScore();
       students.push_back(Student(name, score, grade));
-    }while(name.compare("none"));
- students.pop_back();
+    }
+  //students.pop_back();
 
  for(int i = 0; i < students.size(); i++)
    {
-     cout << students[i].name << endl;
+     cout << students[i].name << "\t" << students[i].score << endl;
    }
   return 0;
 }
 
 float getScore()
 {
-
+  float score;
+  string str;
+  try
+    {
+      cout << "Enter student's score (0 to 100): ";
+      // cin >> score;
+      // score = atof(getline(cin,str).c_str());
+      getline(cin,str);
+      score = atof(str.c_str());
+      if (score < 0 || score > 100)
+	{
+	  throw out_of_range("Score must be a number between 0 and 100");
+	}
+    }
+  catch(out_of_range e)
+    {
+	cout << e.what() << endl;
+	cin.clear();
+	cin.sync();
+	//score = getScore();
+	score = 0.0;
+    }
+  return score;
 }
